b5dcd629d2cd14d831c32690f92a280ad342b08c
Renzo <reemer2002@gmail.com>
2022-05-12 09:33:37 +0200

    fix gitignore

---
+++ SpaceGame/Assets/scripts/pointAndShoot.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointAndShoot : MonoBehaviour
+{
+    private Vector3 target;
+    public GameObject turret;
+    public GameObject crosshairs;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 60.0f;
+    public GameObject bulletstart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshairs.transform.position = new Vector2(target.x, target.y);
+
+        Vector3 difference = target - turret.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        turret.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            float distance = difference.magnitude;
+            Vector2 direction = difference / distance;
+            direction.Normalize();
+            fireBullet(direction, rotationZ);
+        }
+    }
+    void fireBullet(Vector2 direction, float rotationZ)
+    {
+        GameObject b = Instantiate(bulletPrefab) as GameObject;
+        b.transform.position = bulletstart.transform.position;
+        b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        b.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+        Destroy(gameObject, 2);
+    }
+}
