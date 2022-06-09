00e99058014dc09ac1d359bb8e38719770e6bfb3
Renzo <reemer2002@gmail.com>
2022-06-09 10:29:30 +0200

    made camera changing position with player

--- spaceGame2/Assets/scripts/MoveCameraPos.cs
+++ spaceGame2/Assets/scripts/MoveCameraPos.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class MoveCameraPos : MonoBehaviour
 {
     private SpriteRenderer player;
+    private Camera cameraSize;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("player").GetComponent<SpriteRenderer>();
+        cameraSize = GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -16,11 +18,26 @@ public class MoveCameraPos : MonoBehaviour
     {
         if(player.transform.position.y > 5.0f)
         {
-            transform.position = new Vector3(0.0f, 10.0f, -10.0f);
+            transform.position = new Vector3(18.0f, 11.0f, -10.0f);
+            cameraSize.orthographicSize = 8;
         }
         if (player.transform.position.y <= 4.5f)
         {
-            transform.position = new Vector3(0.0f, 0.0f, -10.0f);
+            transform.position = new Vector3(-5.0f, 0.0f, -10.0f);
+            cameraSize.orthographicSize = 9;
+        }
+        if (player.transform.position.x < 2.0f && player.transform.position.y > 4.5f)
+        {
+            transform.position = new Vector3(-16.0f, 11.0f, -10.0f);
+            cameraSize.orthographicSize = 8;
+        }
+        if (player.transform.position.x < 2.0f && player.transform.position.y > 15.0f)
+        {
+            transform.position = new Vector3(-15.0f, 20.0f, -10.0f);
+        }
+        if (player.transform.position.y > 25.0f)
+        {
+            transform.position = new Vector3(-35.5f, 30.0f, -10.0f);
         }
     }
 }
--- spaceGame2/Assets/scripts/onTriggerTEMP.cs
+++ spaceGame2/Assets/scripts/onTriggerTEMP.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class onTriggerTEMP : MonoBehaviour
 {
 
-    public string lever1;
-    public string lever2;
-    public string lever3;
-    public string lever4;
-    public string lever5;
+    [SerializeField] private string lever1;
+    [SerializeField] private string lever2;
+    [SerializeField] private string lever3;
+    [SerializeField] private string lever4;
+    [SerializeField] private string lever5;
+    [SerializeField] private string lever6;
+    [SerializeField] private string lever7;
 
     private bool lever1Trigger = false;
     private bool lever2Trigger = false;
     private bool lever3Trigger = false;
     private bool lever4Trigger = false;
     private bool lever5Trigger = false;
-
-    private float repairCount = 0;
+    private bool lever6Trigger = false;
+    private bool lever7Trigger = false;
 
     private float timer = 0;
     private float waitingTime = 1.0f;
@@ -28,16 +30,20 @@ public class onTriggerTEMP : MonoBehaviour
     private SpriteRenderer leverColor3;
     private SpriteRenderer leverColor4;
     private SpriteRenderer leverColor5;
+    private SpriteRenderer leverColor6;
+    private SpriteRenderer leverColor7;
     [SerializeField] private Sprite backgroundChange;
 
     private void Start()
     {
         spriteRenderer = GameObject.Find("/wall").GetComponent<SpriteRenderer>();
-        leverColor1 = GameObject.Find("/lever1/circle").GetComponent<SpriteRenderer>();
-        leverColor2 = GameObject.Find("/lever2/circle").GetComponent<SpriteRenderer>();
-        leverColor3 = GameObject.Find("/lever3/circle").GetComponent<SpriteRenderer>();
-        leverColor4 = GameObject.Find("/lever4/circle").GetComponent<SpriteRenderer>();
-        leverColor5 = GameObject.Find("/lever5/circle").GetComponent<SpriteRenderer>();
+        leverColor1 = GameObject.Find("/lever1 (lights)/circle").GetComponent<SpriteRenderer>();
+        leverColor2 = GameObject.Find("/lever2 (lights)/circle").GetComponent<SpriteRenderer>();
+        leverColor3 = GameObject.Find("/lever3 (lights)/circle").GetComponent<SpriteRenderer>();
+        leverColor4 = GameObject.Find("/lever4 (lights)/circle").GetComponent<SpriteRenderer>();
+        leverColor5 = GameObject.Find("/lever5 (lights)/circle").GetComponent<SpriteRenderer>();
+        leverColor6 = GameObject.Find("/lever6 (medic)/circle").GetComponent<SpriteRenderer>();
+        leverColor7 = GameObject.Find("/lever7 (medic)/circle").GetComponent<SpriteRenderer>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -60,6 +66,14 @@ public class onTriggerTEMP : MonoBehaviour
         if (collision.gameObject.CompareTag(lever5))
         {
             lever5Trigger = true;
+        }
+        if (collision.gameObject.CompareTag(lever6))
+        {
+            lever6Trigger = true;
+        }
+        if (collision.gameObject.CompareTag(lever7))
+        {
+            lever7Trigger = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -84,6 +98,14 @@ public class onTriggerTEMP : MonoBehaviour
         {
             lever5Trigger = false;
         }
+        if (collision.gameObject.CompareTag(lever6))
+        {
+            lever6Trigger = false;
+        }
+        if (collision.gameObject.CompareTag(lever7))
+        {
+            lever7Trigger = false;
+        }
         timer = 0;
     }
     private void Update()
@@ -91,7 +113,7 @@ public class onTriggerTEMP : MonoBehaviour
         if (leverColor1.color == Color.green && leverColor2.color == Color.green && leverColor3.color == Color.green && leverColor4.color == Color.green && leverColor5.color == Color.green)
         {
             Debug.Log("repaired 5 levers");
-            spriteRenderer.sprite = backgroundChange;
+            //spriteRenderer.sprite = backgroundChange;
         }
 
         if (lever1Trigger == true)
@@ -105,7 +127,6 @@ public class onTriggerTEMP : MonoBehaviour
                     Debug.Log("repaired lever 1");
                     timer = 0;
                     leverColor1.color = Color.green;
-                    repairCount++;
                 }
             }
         }
@@ -120,7 +141,6 @@ public class onTriggerTEMP : MonoBehaviour
                     Debug.Log("repaired lever 2");
                     timer = 0;
                     leverColor2.color = Color.green;
-                    repairCount++;
                 }
             }
         }
@@ -135,7 +155,6 @@ public class onTriggerTEMP : MonoBehaviour
                     Debug.Log("repaired lever 3");
                     timer = 0;
                     leverColor3.color = Color.green;
-                    repairCount++;
                 }
             }
         }
@@ -150,7 +169,6 @@ public class onTriggerTEMP : MonoBehaviour
                     Debug.Log("repaired lever 4");
                     timer = 0;
                     leverColor4.color = Color.green;
-                    repairCount++;
                 }
             }
         }
@@ -165,7 +183,34 @@ public class onTriggerTEMP : MonoBehaviour
                     Debug.Log("repaired lever 5");
                     timer = 0;
                     leverColor5.color = Color.green;
-                    repairCount++;
+                }
+            }
+        }
+        if (lever6Trigger == true)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= waitingTime)
+                {
+                    Debug.Log("repaired lever 6");
+                    timer = 0;
+                    leverColor6.color = Color.green;
+                }
+            }
+        }
+        if (lever7Trigger == true)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                timer += Time.deltaTime;
+
+                if (timer >= waitingTime)
+                {
+                    Debug.Log("repaired lever 7");
+                    timer = 0;
+                    leverColor7.color = Color.green;
                 }
             }
         }
